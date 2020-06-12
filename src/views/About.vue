fbba5d40cfd59d79a9e3679bb971b5f28d5a302d
krisYanachkov <kris.yanachkov@gmail.com>
2020-06-12 17:44:22 +0300

    add views

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>About Morphic Community</h1>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>Morphic Community</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
