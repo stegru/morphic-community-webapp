a5d711dc5c122aac90573233d4765f8ac464665c
krisYanachkov <kris.yanachkov@gmail.com>
2020-06-12 14:32:25 +0300

    initial commit: add clean Vue project structure

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Home msg="Morphic Community"/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {}
+</style>
