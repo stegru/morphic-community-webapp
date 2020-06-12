f3a0e5b806567bf4f20b2a167ded6f31842797a2
krisYanachkov <kris.yanachkov@gmail.com>
2020-06-12 18:27:32 +0300

    add header and footer components

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+<header>
+<div id="nav">
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+</div>
+</header>
+</template>
+
+<script>
+
+export default {
+  name: 'Header'
+}
+</script>
