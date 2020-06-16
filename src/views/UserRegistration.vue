86e3fed5a04e6a86139432fbbb21ca53fda53f24
krisYanachkov <kris.yanachkov@gmail.com>
2020-06-16 08:36:35 +0300

    add registration form

---
+++ src/views/UserRegistration.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <h1>Register</h1>
+    <AuthForm :submitForm="registerUser" buttonText="Register" hasName=true />
+  </v-container>
+</template>
+
+<script>
+
+import AuthForm from '@/components/AuthForm.vue'
+
+export default {
+  components: {
+    AuthForm
+  },
+  methods: {
+    async registerUser (registrationInfo) {}
+  }
+}
+</script>
