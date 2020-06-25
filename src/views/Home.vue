7d676f758258c0d15d39e0845e3ede879593f29f
Danail Karadaliev <danail.karadaliev@gmail.com>
2020-06-25 10:04:38 +0300

    Front-end > Global: Changed the way footer looks; Removed the login column on the right of all pages Front-end > Home: Added the info panels

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,103 @@
 <template>
   <div>
-    <b-row>
-      <b-col cols="9">
-        <h3>What is Morphic Community and how to register</h3>
-        <p>Aliquam mollis maximus enim eget laoreet. Aliquam semper enim eu efficitur fringilla. Integer non ligula tristique, viverra massa in, euismod orci. Morbi tempor urna ante, ut gravida diam rutrum id. Morbi a diam ut nulla pharetra finibus sed in nulla. Vivamus aliquam ex ac ex posuere efficitur. Etiam nulla lacus, vulputate et convallis eget, tempus ac ligula. In in turpis non lorem porta finibus placerat vitae lorem. Donec sit amet tristique justo. Ut nisl nibh, tempor eget sollicitudin sit amet, luctus sed odio. Etiam aliquam mauris pretium nibh maximus, nec placerat erat malesuada. </p>
-        <p>In at varius dui, sed tincidunt lacus. Sed sed dolor non libero tincidunt sagittis. Ut bibendum maximus mi quis eleifend. Ut elit urna, pharetra in est sed, pretium efficitur mauris. Duis at ligula ornare eros hendrerit pellentesque a condimentum diam. Vestibulum sit amet tristique turpis. Nulla eget lectus lectus. Curabitur tincidunt sed urna in porta. Duis sed tristique eros. Nulla porta odio non pulvinar varius. Nunc finibus eros vel porttitor suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus id sem facilisis malesuada. Integer vestibulum varius tincidunt. Etiam tellus leo, scelerisque id malesuada a, iaculis vitae erat. </p>
-        <p>Vivamus commodo, risus et porta cursus, dolor massa semper erat, sit amet blandit est leo sed diam. Aenean risus metus, gravida et mauris ac, congue fringilla ex. Aenean consectetur elit odio, vitae viverra quam congue a. Sed eget neque a ligula commodo iaculis. Ut tempor ultricies elementum. Aliquam nec eros a odio accumsan blandit. Aliquam nec nisl quis tortor maximus imperdiet vitae sit amet massa. Ut ligula velit, sodales vitae malesuada congue, elementum id nisi. Nulla id tincidunt augue. Fusce blandit, urna sed sollicitudin euismod, quam sem ultrices augue, ut efficitur velit quam quis ipsum. Curabitur magna lacus, molestie at nulla finibus, sagittis pulvinar nulla. Aenean volutpat arcu a pretium blandit. </p>
-        <p>
-          <b-link to="/registration" class="btn btn-primary">Register Now</b-link>
-        </p>
-      </b-col>
-      <b-col cols="3">
-        <UserLogin />
-      </b-col>
-    </b-row>
+    <b-jumbotron class="mb-0" id="splash">
+      <b-row>
+        <b-col lg="6"></b-col>
+        <b-col md="12" lg="6" class="text-right text-white">
+          <h1>Welcome to Morphic Community</h1>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices est lacus, id gravida quam placerat vitae.</p>
+          <hr class="my-4">
+          <p>Phasellus tincidunt maximus orci in scelerisque. Nulla enim nisi, dignissim vel quam eu, aliquet efficitur sapien. Morbi eleifend ligula sit amet justo vestibulum euismod.</p>
+          <p>
+            <b-button to="/registration" size="lg" variant="primary" class="mb-2">Register Community</b-button>
+            <b-button to="/registration" size="lg" variant="success" class="mb-2">Download Morphic Lite</b-button>
+          </p>
+        </b-col>
+      </b-row>
+    </b-jumbotron>
+
+    <BlockStatistics />
+
+    <BlockHowItWorks />
+
+    <b-jumbotron class="mt-4" id="intro-1">
+      <b-row>
+        <b-col md="12" lg="6">
+          <h1>What's Morphic Lite?</h1>
+          <p class="lead">
+            Praesent varius risus at sapien fringilla euismod. Vestibulum elementum ultrices nibh, eget fringilla sapien facilisis vel. Donec congue vulputate ipsum sit amet pellentesque. Sed facilisis lacus condimentum quam aliquam eleifend. Phasellus quis lacus at quam gravida condimentum non et augue. Nulla vel eros iaculis, porta magna vel, tincidunt nisi.
+          </p>
+          <p>
+            <b-button to="/about-morphic" size="lg" variant="success">Learn More</b-button>
+          </p>
+        </b-col>
+      </b-row>
+    </b-jumbotron>
+
+    <b-jumbotron id="intro-2">
+      <b-row>
+        <b-col lg="6"></b-col>
+        <b-col md="12" lg="6" class="text-right text-white">
+          <h1>How Morphic Community can help you:</h1>
+          <ul>
+            <li>
+              <h4 class="text-warning">First Reason</h4>
+              <p>Nullam in tincidunt odio. Ut mattis, dui at finibus auctor.</p>
+            </li>
+            <li>
+              <h4 class="text-warning">Second Reason</h4>
+              <p>Pellentesque interdum congue est id tincidunt.</p>
+            </li>
+            <li>
+              <h4 class="text-warning">Third Reason</h4>
+              <p>Morbi finibus pulvinar quam vitae gravida. Vivamus faucibus nulla sodales finibus faucibus.</p>
+            </li>
+            <li>
+              <h4 class="text-warning">Fourth Reason</h4>
+              <p>Duis vel imperdiet diam, sit amet pretium nisi.</p>
+            </li>
+          </ul>
+          <p>
+            <b-button to="/our-communities" size="lg" variant="success">Learn More</b-button>
+          </p>
+        </b-col>
+      </b-row>
+    </b-jumbotron>
   </div>
 </template>
 
+<style>
+  #splash, #intro-1, #intro-2 {
+    background: url(/img/splashFixed.jpg) no-repeat top center;
+    background-size: cover;
+  }
+  #intro-1 {
+    background-image: url(/img/introFixed-1.jpg);
+    background-position: bottom center;
+  }
+  #intro-2 {
+    background-image: url(/img/introFixed-2.jpg);
+  }
+  #intro-2 ul {
+      list-style: none;
+      margin: 2rem 0 0 0;
+      padding: 0;
+  }
+  #intro-2 h4 {
+      margin: 0;
+  }
+</style>
+
 <script>
-import UserLogin from '@/components/UserLogin'
+import BlockStatistics from '@/components/BlockStatistics'
+import BlockHowItWorks from '@/components/BlockHowItWorks'
 
 export default {
   name: 'Home',
+
   components: {
-    UserLogin
+    BlockStatistics,
+    BlockHowItWorks
   }
 }
 </script>
--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,27 +1,18 @@
 <template>
-  <div>
-    <b-row>
-      <b-col cols="9">
-        <h3 class="mb-3">Register your new Comminity</h3>
-        <p>Integer viverra ultricies risus. Donec luctus hendrerit fermentum. Aenean volutpat, nunc quis dictum bibendum, odio nulla euismod ligula, eu tincidunt turpis mauris quis quam. Fusce vitae leo dolor. Proin dapibus purus lobortis pretium porta. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus eget purus vel odio placerat pharetra ac non erat. Sed a dui eget dui imperdiet interdum. Mauris in nisl at augue posuere aliquet vel sit amet orci. Suspendisse dictum neque eget mollis iaculis. </p>
-        <br>
-        <UserRegistration />
-      </b-col>
-      <b-col cols="3">
-        <InfoLogin />
-      </b-col>
-    </b-row>
-  </div>
+  <b-jumbotron class="bg-light">
+    <h3 class="mb-3">Register your new Comminity</h3>
+    <p>Integer viverra ultricies risus. Donec luctus hendrerit fermentum. Aenean volutpat, nunc quis dictum bibendum, odio nulla euismod ligula, eu tincidunt turpis mauris quis quam. Fusce vitae leo dolor. Proin dapibus purus lobortis pretium porta. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus eget purus vel odio placerat pharetra ac non erat. Sed a dui eget dui imperdiet interdum. Mauris in nisl at augue posuere aliquet vel sit amet orci. Suspendisse dictum neque eget mollis iaculis. </p>
+    <br>
+    <UserRegistration />
+  </b-jumbotron>
 </template>
 
 <script>
 import UserRegistration from '@/components/UserRegistration'
-import InfoLogin from '@/components/InfoLogin'
 
 export default {
   components: {
-    UserRegistration,
-    InfoLogin
+    UserRegistration
   }
 }
 </script>
