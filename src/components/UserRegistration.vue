a8e132e4b5c328773163db937971e39c5a1a7a4a
Danail Karadaliev <danail.karadaliev@gmail.com>
2020-06-29 12:21:33 +0300

    Front-end > Registration: Redesigned

--- src/components/UserRegistration.vue
+++ src/components/UserRegistration.vue
@@ -6,13 +6,16 @@
     <b-alert variant="success" :show="successAlert">
       {{ successMessage }}
     </b-alert>
+    <legend>Basic Information</legend>
     <b-form-group>
-      <b-form-input
-        v-model="$v.form.communityName.$model"
-        :state="validateState('communityName')"
-        label="my-community"
-        placeholder="my-community"
-      />
+      <b-input-group append=".morphic-community.com" class="mb-2 mr-sm-2 mb-sm-0">
+        <b-form-input
+          v-model="$v.form.communityName.$model"
+          :state="validateState('communityName')"
+          label="my-community"
+          placeholder="my-community"
+        />
+      </b-input-group>
       <b-form-invalid-feedback>This is a required field.</b-form-invalid-feedback>
     </b-form-group>
     <b-form-group>
@@ -62,19 +65,54 @@
       />
       <b-form-invalid-feedback>This is a required field and must match password.</b-form-invalid-feedback>
     </b-form-group>
-    <b-form-group label="Subscription Plan">
-      <b-form-radio v-model="$v.form.subscriptionPlan.$model" name="bronze" value="bronze">Bronze</b-form-radio>
-      <b-form-radio v-model="$v.form.subscriptionPlan.$model" name="silver" value="silver">Silver</b-form-radio>
-      <b-form-radio v-model="$v.form.subscriptionPlan.$model" name="gold" value="gold">Gold</b-form-radio>
+    <br>
+    <legend>Subscription Plan</legend>
+    <b-form-group id="subscription-plans">
+      <b-form-radio v-model="$v.form.subscriptionPlan.$model" name="bronze" value="bronze">
+        <h5 class="text-bronze"><b>Bronze Tier</b></h5>
+        <h6 class="mb-0"><b>$10</b>/mo for <b>10</b> member community</h6>
+      </b-form-radio>
+      <b-form-radio v-model="$v.form.subscriptionPlan.$model" name="silver" value="silver">
+        <h5 class="text-secondary"><b>Silver Tier</b></h5>
+        <h6 class="mb-0"><b>$15</b>/mo for <b>20</b> member community</h6>
+      </b-form-radio>
+      <b-form-radio v-model="$v.form.subscriptionPlan.$model" name="gold" value="gold">
+        <h5 class="text-warning"><b>Gold Tier</b></h5>
+        <h6 class="mb-0"><b>$20</b>/mo for <b>50</b> member community</h6>
+      </b-form-radio>
       <b-form-invalid-feedback>This is a required field.</b-form-invalid-feedback>
     </b-form-group>
-    <b-form-group label="Subscription Plan">
-      <b-form-radio v-model="$v.form.paymentOptions.$model" name="stripe" value="stripe">Stripe</b-form-radio>
+    <br>
+    <legend>Payment Gateway</legend>
+    <b-form-group id="payment-gateways">
+      <b-form-radio v-model="$v.form.paymentOptions.$model" name="stripe" value="stripe">
+        <img src="/img/payment-icon-stripe.svg" alt="Stripe">
+      </b-form-radio>
       <b-form-invalid-feedback>This is a required field.</b-form-invalid-feedback>
     </b-form-group>
     <b-button type="submit" variant="primary">Create new Community</b-button>
   </b-form>
 </template>
+
+<style>
+  .text-bronze {
+    color: #cd7f32;
+  }
+  #subscription-plans .custom-radio,
+  #payment-gateways .custom-radio {
+    background: white;
+    border: 1px solid #ddd;
+    padding: 1rem 1rem 1rem 2.5rem;
+    margin-bottom: .5rem; 
+  }
+  #subscription-plans label {
+    width: 100%;
+  }
+  #payment-gateways img {
+    height: 2rem;
+    vertical-align: baseline;
+  }
+</style>
 
 <script>
 import { validationMixin } from 'vuelidate'
