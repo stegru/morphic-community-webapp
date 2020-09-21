52d8615cbafe36d84bb684560752c38545db89b8
Javier Hernández <javi@raisingthefloor.org>
2020-09-21 19:28:01 +0200

    MOR-310: Updated Header to include a link to the new Focused UI

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,8 @@
       </b-navbar-brand>
 
       <b-navbar-nav class="mr-auto">
-        <b-nav-item to="/dashboard/" v-if="isLoggedIn" exact-active-class="active"><b>Dashboard</b></b-nav-item>
+        <b-nav-item to="/dashboard/" v-if="isLoggedIn" exact-active-class="active"><b>Dashboard Mode</b></b-nav-item>
+        <b-nav-item to="/focused/home" v-if="isLoggedIn" exact-active-class="active"><b>Focus/Mobile Mode</b></b-nav-item>
       </b-navbar-nav>
 
       <b-navbar-nav>
