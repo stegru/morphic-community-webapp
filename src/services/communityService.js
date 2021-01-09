72768ae4f494806b6a8f51cbb0e8e0577975dbc8
ste <git@grundell.co.uk>
2021-01-09 21:12:34 +0000

    MOR-123: Added jsdoc to some service functions.

--- src/services/communityService.js
+++ src/services/communityService.js
@@ -1,9 +1,25 @@
 import { HTTP } from "@/services/index";
 
+/**
+ * @typedef {String} GUID
+ */
+
+/**
+ * Get a list of communities the user belongs to.
+ * @see https://github.com/raisingthefloor/morphic-api-server/blob/master/Documentation/API.md#v1usersidcommunities
+ * @param {GUID} userId The user ID.
+ * @return {Promise<AxiosResponse<Any>>} Response
+ */
 export function getUserCommunities(userId) {
     return HTTP.get(`/v1/users/${userId}/communities`);
 }
 
+/**
+ * Creates a new community.
+ * @see https://github.com/raisingthefloor/morphic-api-server/blob/master/Documentation/API.md#v1communities
+ * @param {String} name Community name.
+ * @return {Promise<AxiosResponse<Any>>} Response.
+ */
 export function createNewCommunity(name) {
     const data = {
         name: name
@@ -11,33 +27,90 @@ export function createNewCommunity(name) {
     return HTTP.post("/v1/communities", data);
 }
 
+/**
+ * Gets a community details.
+ * @see https://github.com/raisingthefloor/morphic-api-server/blob/master/Documentation/API.md#v1communitiesid
+ * @param {GUID} communityId The community ID.
+ * @return {Promise<AxiosResponse<Any>>} Response
+ */
 export function getCommunity(communityId) {
     return HTTP.get(`/v1/communities/${communityId}`);
 }
 
+/**
+ * Delete a community.
+ * @see https://github.com/raisingthefloor/morphic-api-server/blob/master/Documentation/API.md#delete
+ * @param {GUID} communityId The community ID.
+ * @return {Promise<AxiosResponse<Any>>} Response
+ */
 export function deleteUserCommunity(communityId) {
     return HTTP.delete(`/v1/communities/${communityId}`);
 }
 
+/**
+ * Gets the bars in a community.
+ * @see https://github.com/raisingthefloor/morphic-api-server/blob/master/Documentation/API.md#v1communitiesidbars
+ * @param {GUID} communityId The community ID.
+ * @return {Promise<AxiosResponse<Any>>} Response
+ */
 export function getCommunityBars(communityId) {
     return HTTP.get(`/v1/communities/${communityId}/bars`);
 }
 
+/**
+ * Creates a community bar.
+ * @see https://github.com/raisingthefloor/morphic-api-server/blob/master/Documentation/API.md#post-13
+ * @param {GUID} communityId The community ID.
+ * @param {BarDetails} data The bar.
+ * @return {Promise<AxiosResponse<Any>>} Response
+ */
 export function createCommunityBar(communityId, data) {
     return HTTP.post(`/v1/communities/${communityId}/bars`, data);
 }
 
+/**
+ * Gets a community bar.
+ * @see https://github.com/raisingthefloor/morphic-api-server/blob/master/Documentation/API.md#v1communitiescidbarsid
+ * @param {GUID} communityId The community ID.
+ * @param {GUID} barId The bar ID.
+ * @return {Promise<AxiosResponse<Any>>} Response
+ */
 export function getCommunityBar(communityId, barId) {
     return HTTP.get(`/v1/communities/${communityId}/bars/${barId}`);
 }
 
+/**
+ * Saves a community bar.
+ * @see https://github.com/raisingthefloor/morphic-api-server/blob/master/Documentation/API.md#put-4
+ * @param {GUID} communityId The community ID.
+ * @param {GUID} barId The bar ID.
+ * @param {BarDetails} barDetails The bar.
+ * @return {Promise<AxiosResponse<Any>>} Response
+ */
 export function saveCommunityBar(communityId, barId, barDetails) {
     return HTTP.put(`/v1/communities/${communityId}/bars/${barId}`, barDetails);
 }
 
+/**
+ * Updates a community bar.
+ * (appears to be a duplicate of saveCommunityBar)
+ * @see https://github.com/raisingthefloor/morphic-api-server/blob/master/Documentation/API.md#put-4
+ * @param {GUID} communityId The community ID.
+ * @param {GUID} barId The bar ID.
+ * @param {BarDetails} bar The bar.
+ * @return {Promise<AxiosResponse<Any>>} Response
+ */
 export function updateCommunityBar(communityId, barId, bar) {
     return HTTP.put(`/v1/communities/${communityId}/bars/${barId}`, bar);
 }
+
+/**
+ * Deletes a community bar.
+ * @see https://github.com/raisingthefloor/morphic-api-server/blob/master/Documentation/API.md#delete-2
+ * @param {GUID} communityId The community ID.
+ * @param {GUID} barId The bar ID.
+ * @return {Promise<AxiosResponse<Any>>} Response
+ */
 export function deleteCommunityBar(communityId, barId) {
     return HTTP.delete(`/v1/communities/${communityId}/bars/${barId}`);
 }
